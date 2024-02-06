c344144b9c2d4052fc734ed4349c8a0efd3612e2
Durga Karthik Yandrapu <durgakarthikyandrapu@gmail.com>
2024-02-06 17:29:11 +0530

    Update Polyalphabetic_Cipher.c

--- Polyalphabetic_Cipher.c
+++ Polyalphabetic_Cipher.c
@@ -8,15 +8,8 @@ void initializeMap(char map[MAX][MAX]) {
     }
   }
 }
-int main() {
-  char map[MAX][MAX];
-  initializeMap(map);
-  char str[30], key[20], decrypted[30];
+void encrypt(char map[MAX][MAX], char str[30], char key[20]) {
   int n, m;
-  printf("Enter the plain text (in Upper case only): ");
-  scanf("%s", str);
-  printf("Enter the key value (in Upper case only): ");
-  scanf("%s", key);
   int keyLength = strlen(key);
   printf("\nEncrypted Text: ");
   for (int i = 0; i < strlen(str); i++) {
@@ -29,4 +22,30 @@ int main() {
     }
     printf("%c", map[n % MAX][m % MAX]);
   }
+}
+void decrypt(char map[MAX][MAX], char str[30], char key[20]) {
+  int n, m;
+  int keyLength = strlen(key);
+  printf("\nDecrypted Text: ");
+  for (int i = 0; i < strlen(str); i++) {
+    n = m = 0;
+    while (map[n % MAX][m % MAX] != str[i]) {
+      n++;
+    }
+    while (m + 'A' != key[i % keyLength]) {
+      m++;
+    }
+    printf("%c", 'A' + n % MAX);
+  }
+}
+int main() {
+  char map[MAX][MAX];
+  initializeMap(map);
+  char str[30], key[20];
+  printf("Enter the plain text (in Upper case only): ");
+  scanf("%s", str);
+  printf("Enter the key value (in Upper case only): ");
+  scanf("%s", key);
+  encrypt(map, str, key);
+  decrypt(map, str, key);
 }
