a9c1daa6ae146702259eacceb1ec525033a24f5d
Durga Karthik Yandrapu <durgakarthikyandrapu@gmail.com>
2024-02-18 19:39:43 +0530

    Update Monoalphabetic_Cipher.c

--- Monoalphabetic_Cipher.c
+++ Monoalphabetic_Cipher.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX 20
+#define MAX 27
 int main() {
     int i;
     char str[MAX], str1[MAX], str2[MAX];
-    char alpha[27] = "abcdefghijklmnopqrstuvwxyz";
+    char alpha[MAX] = "abcdefghijklmnopqrstuvwxyz";
     printf("\nPlain  Alphabets: %s", alpha);
     srand(time(NULL));
     for (i = 25; i > 0; i--) {
@@ -15,8 +15,9 @@ int main() {
         alpha[j] = temp;
     }
     printf("\nCipher Alphabets: %s", alpha);
-    printf("\nEnter String: ");
-    gets(str);
+    printf("\nEnter the plain text (in Lower case only): ");
+    fgets(str, MAX, stdin);
+    str[strcspn(str, "\n")] = '\0';
     // Encryption
     for (i = 0; str[i]; i++) {
         if (islower(str[i])) {
@@ -36,6 +37,6 @@ int main() {
         str2[i] = 'a' + index;
     }
     str2[i] = '\0';
-    printf("\nEncrypted String: %s", str1);
-    printf("\nDecrypted String: %s\n", str2);
+    printf("\nEncrypted Text: %s", str1);
+    printf("\nDecrypted Text: %s\n", str2);
 }
